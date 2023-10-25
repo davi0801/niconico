cee4033a363bf65ab92c05479b42fb5ec957193e
davi0801 <113618958+davi0801@users.noreply.github.com>
2023-10-25 12:34:47 -0300

    Update main.js

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -4,4 +4,15 @@ function tocaSomPom () {
 
 document.querySelector('.tecla_pom').onclick = tocaSomPo
 
-const listaDeTeclas = document.querySelectorall('.tecla')
+
+const listaDeTeclas = document.querySelectorall('.tecla')  
+
+let contador = 0;
+
+//while significa enquanto
+while (contador <9) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
