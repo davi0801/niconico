8d10aa4ace6f83042e10ec82bbd701edb8401c67
davi0801 <113618958+davi0801@users.noreply.github.com>
2023-11-01 12:35:11 -0300

    Update main.js

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPo
@@ -10,10 +10,14 @@ const listaDeTeclas = document.querySelectorall('.tecla')
 let contador = 0;
 
 //while significa enquanto
-while (contador <9) {
-
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador = contador + 1;
+while (contador < listaDeTeclas.length) {
+ const efeito = listaDeTeclas [contador].classList[1];
+const idAudio = "#som_" +efeito;
+    listaDeTeclas[contador].onclick = function ()
+        {
+            tocaSom (idAudio);
+        }
+            contador = contador + 1;
     console.log(contador);
 }
 // Pom ----------------------------------------
